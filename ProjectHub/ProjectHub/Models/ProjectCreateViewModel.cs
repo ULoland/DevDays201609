d7780ca52acb2288eeaf5ea65fcca7e5d40cc9fd
marconiemi <marco.niemi@nansen.com>
2016-09-30 15:39:35 +0200

    Add topics

---
+++ ProjectHub/ProjectHub/Models/ProjectCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectHub.Models
+{
+    public class ProjectCreateViewModel
+    {
+        public ProjectModel projectModel {get;set;}
+        public List<TopicModel> availableTopics { get; set; }
+        public List<PostModel> posts { get; set; }
+    }
+}
