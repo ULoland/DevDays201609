fc359ab70d0605455bf8b0eda9a28327dd79ef88
Ulf Loland <ulfloland@outlook.com>
2016-09-30 11:16:32 +0200

    Initial checkin

---
+++ ProjectHub/ProjectHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectHub.Startup))]
+namespace ProjectHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
