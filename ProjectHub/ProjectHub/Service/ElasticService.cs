a5140d1e29972ba3567a1670345768386fb3bc93
babadofar <christoffer.vig@gmail.com>
2016-09-30 14:44:00 +0200

    fixed return id for projects

--- ProjectHub/ProjectHub/Service/ElasticService.cs
+++ ProjectHub/ProjectHub/Service/ElasticService.cs
@@ -48,7 +48,11 @@ namespace ProjectHub.Service
 		public ProjectModel  GetProject (string projectName)
 		{
 			var res = _elastic.Search<ProjectModel>(q => q.Query(qm => qm.Term("name.keyword", projectName)));
-			var projectids = res.Hits.Select(m => m.Source).FirstOrDefault();
+			var projectids = res.Hits.Select(m =>
+			{
+				m.Source.Id = m.Id;
+				                                      return m.Source;
+			}).FirstOrDefault();
 			return projectids;
 		}
 	}
