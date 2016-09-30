8d77ae777dc143d9479b4cb030a8e95ac4f34509
Ulf Loland <ulfloland@outlook.com>
2016-09-30 11:58:33 +0200

    update

--- ProjectHub/ProjectHub/Controllers/PostController.cs
+++ ProjectHub/ProjectHub/Controllers/PostController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProjectHub.Models;
 
 namespace ProjectHub.Controllers
 {
@@ -17,7 +18,13 @@ namespace ProjectHub.Controllers
         [HttpGet]
         public ActionResult Create()
         {
-            return View()
+            var model = new PostModel();
+            return View(model);
+        }
+
+        public ActionResult Create(PostModel model)
+        {
+            return View(model);
         }
     }
 }
