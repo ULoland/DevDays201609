4ad651e26eeff6a40902f33bf63ab4c8f0ef54a5
Konrad Lalik <konrad.lalik@makingwaves.com>
2016-09-30 16:22:37 +0200

    update

--- ProjectHub/ProjectHub/Controllers/SearchController.cs
+++ ProjectHub/ProjectHub/Controllers/SearchController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProjectHub.Service;
 
 namespace ProjectHub.Controllers
 {
     public class SearchController : Controller
     {
+        private readonly SearchService _searchService;
+
+        public SearchController()
+        {
+            _searchService = new SearchService();
+        }
+
         // GET: Search
         public ActionResult Index(string query)
         {
-            return View();
+            var result = _searchService.Search(query);
+
+            return View(result);
         }
     }
 }
